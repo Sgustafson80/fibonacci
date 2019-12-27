edccea0cc508b795079097f7668666df180472cc
Sarah Gustafson <sgustafson80@gmail.com>
2019-12-27 20:15:20 +0000

    working recursive

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,16 +1,15 @@
 def recursive_fib (n)
-  if n == 0
-    return 0
-  elsif n==1
-    return 1
+  if n == 0 || n==1
+    return n
   else
     return recursive_fib(n-1) + recursive_fib(n-2)
   end
 end
 
-puts recursive_fib(35)
+puts recursive_fib(9)
 
 def iterative_fib (n)
-  i = 0
-  
+ 
 end
+
+puts iterative_fib(9)
