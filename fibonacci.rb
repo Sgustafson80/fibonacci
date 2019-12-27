88d5fe2d9fc8b67b4ec438d398c12c32d648027a
Sarah Gustafson <sgustafson80@gmail.com>
2019-12-27 20:31:40 +0000

    iterative and benchmark

--- fibonacci.rb
+++ fibonacci.rb
@@ -6,10 +6,26 @@ def recursive_fib (n)
   end
 end
 
-puts recursive_fib(9)
+puts recursive_fib(35)
 
 def iterative_fib (n)
- 
+  a = 0
+  b = 1
+
+  n.times do
+    temp = a
+    a = b
+    b = temp + b
+  end
+
+  return a
 end
 
-puts iterative_fib(9)
+puts iterative_fib(35)
+
+require 'benchmark'
+num = 35
+Benchmark.bm do |x|
+  x.report("recursive_fib") { recursive_fib(num) }
+  x.report("iterative_fib")  { iterative_fib(num)  }
+end
