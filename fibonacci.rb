d4df7d8f729947cf68e801119dcb35c554848b97
Sarah Gustafson <sgustafson80@gmail.com>
2019-12-27 20:05:26 +0000

    initial commit

---
+++ fibonacci.rb
@@ -0,0 +1,16 @@
+def recursive_fib (n)
+  if n == 0
+    return 0
+  elsif n==1
+    return 1
+  else
+    return recursive_fib(n-1) + recursive_fib(n-2)
+  end
+end
+
+puts recursive_fib(35)
+
+def iterative_fib (n)
+  i = 0
+  
+end
